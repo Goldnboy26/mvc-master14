701f45ede0645115899e445df04b981906a15919
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-10-12 21:15:05 -0700

    make it better

--- config/connection.js
+++ config/connection.js
@@ -15,5 +15,44 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
-
 module.exports = sequelize;
+
+if (process.env.JAWSDB_URL) {
+       sequelize = new Sequelize(process.env.JAWSDB_URL);
+   } else {
+       sequelize = new Sequelize
+       process.env.DB_NAME, 
+       process.env.DB_USER, 
+       process.env.DB_PW, 
+       {
+           host: process.env.DB_HOST,
+           port: process.env.DB_PORT,
+           dialect: 'mysql',
+       };
+   }
+
+  
+  //to test the connection 
+   sequelize
+       .authenticate()
+       .then(() => {
+           console.log('Connection has been established successfully.');
+       })
+       .catch(err => {
+          console.error('Unable to connect to the database:', err);
+       });
+  
+  
+   if (process.env.JAWSDB_URL) {
+       sequelize = new Sequelize(process.env.JAWSDB_URL)
+   } else {
+       sequelize = new Sequelize(
+           process.env.DB_APP_DATABASE,
+           process.env.DB_USER,
+           process.env.DB_PASS, {
+               host: process.env.DB_HOST,
+               port: process.env.DB_PORT,
+               dialect: 'mysql',
+         });
+   }
+  
