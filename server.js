daa9743002ac48c80a6b94f8586c1c41115da365
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-10-13 19:29:36 -0700

    make it better

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SESSION_SECRET,
+    secret: session,
     cookie: {
         maxAge: 280
     },
