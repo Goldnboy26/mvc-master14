b5e26494a862e770302b623b77ea479ed21d0de3
Goldnboy26 <enrique.monje1994@yahoo.com>
2022-10-14 19:55:11 -0700

    connect to jawsdb

--- server.js
+++ server.js
@@ -1,61 +1,47 @@
-// Dependencies
+// import packages and files
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = require('hbs');
-const routes = require('./controllers/');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 
+
+//setup handlebars helpers
+const hbs = exphbs.create({ helpers });
+
+//setup session
+const sess = {
+  secret: 'secret secret',
+  cookie: { maxAge: 7200000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+//setup port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-    secret: process.env.DB_NAME,
-    cookie: {
-        maxAge: 280
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
-
-app.use(session(sess));
-hbs = exphbs.create({
-    helpers
-});
-app.engine('handlebars', hbs.engine);
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('view engine', 'hbs');
-app.use(session(sess));
-
-app.use(express.json());
-
-app.use(express.urlencoded
-    ({
-        extended: true
-    }));
-
 app.use(express.static(path.join(__dirname, 'public')));
-//path to the routes
+
+//setup handlebars template engine 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session(sess));
+
 app.use(routes);
 
-console.log(`Database name: ${sequelize.config.tech_blog_db} \n running on port: ${sequelize.config.PORT} \n 
-under hostname: ${sequelize.config.localhost}`);
-
-sequelize.sync({
-    force: false
-}).then(() => {
-    app.listen(PORT, () => {
-        console.log(`App listening on port ${PORT}!`);
-    }
-    )
-})
-    ;
+//initialize server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
+});
